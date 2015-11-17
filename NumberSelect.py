3aee12b7904677ddee5e5a71b96cbb778f004122
z2 <Two@Ms-MacBook-Pro.local>
2015-11-17 13:53:41 -0500

    simplified and commented

--- NumberSelect.py
+++ NumberSelect.py
@@ -1,14 +1,28 @@
+# NumberSelect.py
+
+"""for each number 1-10 pick random number ("pick") and see if there is a match
+     if there is a match increase "match" by one
+     divide total matches over specified number of rounds and divide by total rounds
+"""
+
 import random
 
-def compare(counter):
+def compare(rounds):
     match = 0
-    b = random.randint(1,10)
-    if counter == b:
-        print("Match: ",counter, b)
 
 
+    for round in range(rounds):
+        for counter in range(1,11):
+            pick = random.randint(1,10)
+            if counter == pick:
+                match += 1
+                # print("****Match: ",counter, pick)
 
-counter = 1
-for n in range(1,10):
-    compare(counter)
-    counter += 1
+            # else:
+                # print("No Match: ", counter, pick)
+
+    print("Total Matches: ",match)
+    print("Rounds: ", rounds)
+    print("Avg Matches Per Round: ", (match/rounds))
+
+compare(20000)
