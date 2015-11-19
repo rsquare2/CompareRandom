3a031c24aa589a0ab8fe5975535d3a960b0d3625
rsquare2 <rsquared90@hotmail.com>
2015-11-19 17:38:13 -0500

    added vars to specify number of times there are 1, 2 & 3 matches

--- NumberSelect.py
+++ NumberSelect.py
@@ -13,8 +13,10 @@ class Compare:
         self.rounds = rounds
         self.match = 0
         self.maxMatchesSoFar = 0
+        self.match1 = 0
+        self.match2 = 0
+        self.match3 = 0
         
-
 
     def selectRandom(self):
         for round in range(self.rounds):
@@ -24,13 +26,26 @@ class Compare:
                 if counter == pick:
                     self.match += 1
                     self.maxMatches +=1
+                            
                 if self.maxMatches > self.maxMatchesSoFar:
                     self.maxMatchesSoFar = self.maxMatches
+
+            if self.maxMatches == 1:
+                   self.match1 += 1
+            if self.maxMatches == 2:
+                   self.match2 += 1
+            if self.maxMatches == 3:
+                   self.match3 += 1
+ 
 
         print("Total Matches: ",self.match)
         print("Rounds: ", self.rounds)
         print("Avg Matches Per Round: ", (self.match/self.rounds))
         print("Max Matches in a Round: ", self.maxMatchesSoFar)
+        print("1 Match Rounds: {0}%".format(int((self.match1/self.rounds)*100)))
+        print("2 Match Rounds: {0}%".format(int((self.match2/self.rounds)*100)))
+        print("3 Match Rounds: {0}%".format(int((self.match3/self.rounds)*100)))
+                
 def main():
     a = Compare(100000)
     b = a.selectRandom()
